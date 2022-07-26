df8ed59af0510f76494f8a87b6bf4e6f5e29dc6e
Hugo Brilhante <hugobrilhante@gmail.com>
2022-07-26 11:17:51 -0300

    Accept ssl configs

--- django_outbox_pattern/factories.py
+++ django_outbox_pattern/factories.py
@@ -16,6 +16,24 @@ def factory_connection():
         heartbeats=heartbeats,
         vhost=vhost,
     )
+    use_ssl = settings.DEFAULT_STOMP_USE_SSL
+    if use_ssl:
+        key_file = settings.DEFAULT_STOMP_KEY_FILE
+        cert_file = settings.DEFAULT_STOMP_CERT_FILE
+        ca_certs = settings.DEFAULT_STOMP_CA_CERTS
+        cert_validator = settings.DEFAULT_STOMP_CERT_VALIDATOR
+        ssl_version = settings.DEFAULT_STOMP_SSL_VERSION
+        password = settings.DEFAULT_STOMP_SSL_PASSWORD
+        connection.set_ssl(
+            for_hosts=host_and_ports,
+            key_file=key_file,
+            cert_file=cert_file,
+            ca_certs=ca_certs,
+            cert_validator=cert_validator,
+            ssl_version=ssl_version,
+            password=password,
+        )
+
     return connection
 
 
