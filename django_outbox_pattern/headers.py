42c0d29808ebe465f57797d29c22a5a3c807a8db
Hugo Brilhante <hugobrilhante@gmail.com>
2022-09-09 16:11:16 -0300

    Rename headers default

--- django_outbox_pattern/headers.py
+++ django_outbox_pattern/headers.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 
 def generate_headers(message):
     return {
-        "msg-id": str(message.id),
-        "msg-destination": message.destination,
-        "msg-type": message.__class__.__name__,
-        "msg-sent-time": timezone.now(),
+        "dop-msg-id": str(message.id),
+        "dop-msg-destination": message.destination,
+        "dop-msg-type": message.__class__.__name__,
+        "dop-msg-sent-time": timezone.now(),
     }
